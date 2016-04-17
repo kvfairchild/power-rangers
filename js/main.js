cd8a122203cee55678048fce0cdabd7eaeae1ca0
Katherine Gallagher <katherinegallagher@fas.harvard.edu>
2016-04-17 10:47:15 -0400

    Replaced KVG files with James updated files

--- js/main.js
+++ js/main.js
@@ -1,14 +1,58 @@
-// Variable for the visualization instance
-var plantExplorer;
+
+// Will be used to the save the loaded JSON data
+var allData = [];
+
+// Date parser to convert strings to date objects
+var parseDate = d3.time.format("%Y").parse;
+
+// Set ordinal color scale
+var colorScale = d3.scale.category20();
+
+// Variables for the visualization instances
+var areachart, timeline;
+var brush, xFocus, x2;
+
 
 // Start application by loading the data
+loadData();
 
-$.getJSON("data/plants.json", createVis);
+function loadData() {
+	d3.json("data/uk-household-purchases.json", function(error, jsonData){
+		if(!error){
+			allData = jsonData;
 
-function createVis(data) {
+			// Convert Pence Sterling (GBX) to USD and years to date objects
+			allData.layers.forEach(function(d){
+				for (var column in d) {
+					if (d.hasOwnProperty(column) && column != "Year") {
+					  d[column] = parseFloat(d[column]) * 1.481105 / 100;
+					} else if(d.hasOwnProperty(column) && column == "Year") {
+						d[column] = parseDate(d[column].toString());
+					}
+				}
+			});
 
-    plantsData = data;
+			allData.years.forEach(function(d){
+				d.Expenditures = parseFloat(d.Expenditures) * 1.481105 / 100;
+				d.Year = parseDate(d.Year.toString());
+			});
 
-  plantExplorer = new plantExplorer("plant-explorer", data, [37.0902, -95.7129]);
 
+			// Update color scale (all column headers except "Year")
+			// We will use the color scale later for the stacked area chart
+			colorScale.domain(d3.keys(allData.layers[0]).filter(function(d){ return d != "Year"; }))
+
+			createVis();
+		}
+	});
+}
+
+function createVis() {
+	areachart = new StackedAreaChart("stacked-area-chart", allData.layers);
+}
+
+
+function brushed() {
+	xFocus.domain(brush.empty() ? x2.domain() : brush.extent());
+	areachart.wrangleData();
 }
